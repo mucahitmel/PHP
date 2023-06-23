44ada4c3b2d4ebbfc01112dbb5a3e573fe867dfa
muco <mucahitmeletli@gmail.com>
2023-06-23 23:57:11 +0300

    first commt

---
+++ PHP.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
+
+// Connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+// Close connection
+$conn->close();
+?>
